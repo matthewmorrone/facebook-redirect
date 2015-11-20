f9aabc9d30d5e0539a53bbbd00c49ad6fafec23a
Matthew Morrone <matthewmorrone1@gmail.com>
2015-11-20 17:28:46 -0500

    fixed bug that prevented link navigation via facebook's redirect

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // if (window.location.host === "www.facebook.com") {
-if (!window.location.pathname.startsWith("/messages")) {
+if (!window.location.pathname.startsWith("/messages") && !window.location.pathname.startsWith("/l.php")) {
 	window.location.href = "https://www.facebook.com/messages";
 }
 // }

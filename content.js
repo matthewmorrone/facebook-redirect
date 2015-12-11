04bea244bf702b21bb8b2a9609e196ab31613d40
Matthew Morrone <matthewmorrone1@gmail.com>
2015-12-11 17:59:42 -0500

    housekeeping, and changed redirect to go to messenger.com

--- content.js
+++ content.js
@@ -1,44 +1,10 @@
+
+
+// var url = "https://www.facebook.com/messages"
+var url = "https://www.messenger.com/t/me"
+
 if (window.location.host === "www.facebook.com") {
 	if (!window.location.pathname.startsWith("/messages") && !window.location.pathname.startsWith("/l.php")) {
-		window.location.href = "https://www.facebook.com/messages";
+		window.location.href = url;
 	}
-	countdown(0, 30, function(t) {
-		if (t === 15) {
-			invert()
-		}
-		// console.log("tick", t)
-	}, function() {
-		$("*").remove()
-		// console.log("done")
-	})
-}
-function invert() {
-	var css = 'body {-webkit-filter: invert(100%);' + '-moz-filter: invert(100%);' + '-o-filter: invert(100%);' + '-ms-filter: invert(100%); background-color: black;}',
-	head = document.getElementsByTagName('head')[0],
-	style = document.createElement('style');
-	style.type = 'text/css';
-	style.styleSheet.cssText = css;
-	head.appendChild(style);
-}
-function countdown(minutes, seconds, tick, done) {
-	tick = tick || function(t) {
-		console.log("tick", t)
-	}
-	done = done || function() {
-		console.log("done")
-	}
-	interval = setInterval(function() {
-		if (seconds == 0) {
-			if (minutes == 0) {
-				done()
-				clearInterval(interval);
-				return;
-			} else {
-				minutes--;
-				seconds = 60;
-			}
-		}
-		tick(seconds)
-		seconds--;
-	}, 1000);
 }
